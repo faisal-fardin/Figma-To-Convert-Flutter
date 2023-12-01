e514c75169775f0d902054916dd907e0b23b6536
niloy <faisalfardin77@gmaul.com>
2023-12-01 09:39:53 +0600

    something

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         textTheme: GoogleFonts.urbanistTextTheme(),
